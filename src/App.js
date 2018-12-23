5d515ad1cbd8df5926f6f9abad2879ac2dce9159
ledaian41 <ledaian41@gmail.com>
2018-12-23 09:06:38 +0700

    seperate into 2 screens

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import Calculator from './components/Calculator'
 import Test1 from './components/Test1'
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			isTest1 : true,
+		}
+	}
+
   render() {
     return (
       <div>
-      	  <Calculator />
-          <Test1 />
+      	{this.display()}
+      	<button onClick={() => {
+      		this.setState({ isTest1: !this.state.isTest1 })
+      	}}>{this.state.isTest1 ? 'Calculator' : 'Test 1'}</button>
       </div>
     );
+  }
+
+  display() {
+  	return this.state.isTest1 ? <Test1 /> : <Calculator />
   }
 }
 
